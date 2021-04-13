3e5a3eb481c0e4c65c47b0620745a4fd641bf57f
97Fakhreddine <fakhri19971998@gmail.com>
2021-04-13 14:31:32 +0100

    adding some styling to signup

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import Spinner from "./components/spinner/Spinner";
 import "./style/App.css";
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   BrowserRouter,
   RouteComponentProps,
 } from "react-router-dom";
@@ -13,29 +10,26 @@ import routes from "./components/config/route";
 const App: React.FC = () => {
   return (
     <div className="App">
-      Typescript
-      <div>
-        <BrowserRouter>
-          <Switch>
-            {routes.map((route, index) => {
-              return (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  render={(props: RouteComponentProps<any>) => (
-                    <route.component
-                      name={route.name}
-                      {...props}
-                      {...route.props}
-                    />
-                  )}
-                />
-              );
-            })}
-          </Switch>
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                render={(props: RouteComponentProps<any>) => (
+                  <route.component
+                    name={route.name}
+                    {...props}
+                    {...route.props}
+                  />
+                )}
+              />
+            );
+          })}
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
--- chat/src/components/pages/Signup.tsx
+++ chat/src/components/pages/Signup.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FormControl, InputBase } from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import "../../style/signup.css";
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(5),
+        width: "25ch",
+        position: "relative",
+        display: "flex",
+        bottom: theme.spacing(1),
+      },
+    },
+  })
+);
+
 const Signup: React.FC<{}> = () => {
+  const classes = useStyles();
   return (
     <div>
       <h1>Sign up now</h1>
-      <div>he is the block</div>
+      <div className="form">
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="standard-basic" label="Username" color="primary" />
+          <TextField id="standard-basic" label="Email" />
+          <TextField id="standard-basic" label="Password" color="secondary" />
+        </form>
+      </div>
+      <div>
+        <Link to="/">go home</Link>
+      </div>
     </div>
   );
 };
